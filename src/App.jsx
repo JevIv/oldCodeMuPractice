63ba221614f01126d4ff7bf77516f7c8d5cadcae
Evgeny Ivanov <jevgenijs240693@gmail.com>
2021-12-16 09:37:12 +0000

    Added new Array

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import Map from "./components/13.12.21/Map";
 import Array2 from "./components/15.12.21/Array2";
 import Array3 from "./components/15.12.21/Array3";
+import Array4 from "./components/16.12.21/Array4";
 import "./styles.css";
 
 export default function App() {
   //<Map />
   //<Array1 />
+  //<Array2 />
+  //<Array3 />
   return (
     <div>
-      <Array2 />
       <hr />
-      <Array3 />
+      <Array4 />
+      <hr />
     </div>
   );
 }
---
+++ src/components/16.12.21/Array4.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+
+export default function Array3() {
+  let id = Math.random();
+  const initNotes = [
+    {
+      id: id,
+      prop1: "value1",
+      prop2: "value2",
+      prop3: "value3"
+    },
+    {
+      id: id,
+      prop1: "value1.2",
+      prop2: "value2.2",
+      prop3: "value3.2"
+    },
+    {
+      id: id,
+      prop1: "value1.3",
+      prop2: "value2.3",
+      prop3: "value3.3"
+    }
+  ];
+
+  const [notes, setNotes] = useState(initNotes);
+
+  const [obj, setObj] = useState(getInitObj());
+
+  function remItem(id) {
+    setNotes(notes.filter((note) => note.id !== id));
+  }
+
+  function getInitObj() {
+    return {
+      id: id,
+      prop1: "",
+      prop2: "",
+      prop3: ""
+    };
+  }
+
+  function addItem() {
+    setNotes([...notes, obj]);
+    setObj(getInitObj());
+  }
+
+  function changeProp(prop, event) {
+    setObj({ ...obj, [prop]: event.currentTarget.value });
+  }
+
+  const result = notes.map((note) => {
+    return (
+      <p key={note.id}>
+        <span>{note.prop1}</span>,<span> {note.prop2}</span>,
+        <span> {note.prop3}</span>,
+        <button onClick={() => remItem(note.id)}>remove</button>
+      </p>
+    );
+  });
+
+  return (
+    <div>
+      {result}
+      <br />
+      <input
+        value={obj.prop1}
+        onChange={(event) => changeProp("prop1", event)}
+      />
+      <input
+        value={obj.prop2}
+        onChange={(event) => changeProp("prop2", event)}
+      />
+      <input
+        value={obj.prop3}
+        onChange={(event) => {
+          changeProp("prop3", event);
+        }}
+      />
+      <button onClick={addItem}>save</button>
+    </div>
+  );
+}
